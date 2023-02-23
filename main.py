2ce4a4f81287155c89b670c992d7f5769d85c23f
Neefs <69609852+Neefs@users.noreply.github.com>
2023-02-23 10:16:56 -0500

    reset to alpha

--- main.py
+++ main.py
@@ -1,31 +1,58 @@
 import asyncio
+import logging
+import os
+import sys
+
 import discord
+from dotenv import load_dotenv
 
-from discord.ext import commands
-from utils.bot import PlaceHolder
-import json
+from objects.bot import Placeholder
+from objects.command_tree import CommandTree
+from objects.discord_changes import Embed, View
 
-with open("config.json", "r") as readfile:
-    config = json.loads(readfile.read())
+if not os.path.exists("logs"):
+    os.mkdir("logs")
+
+load_dotenv(".env")
 
 
-async def main():
-    bot = PlaceHolder(config=config, intents=discord.Intents.all())
+"""_summary_
+This will be the static logging for discord.
+"""
+logger = logging.getLogger("discord")
+logger.setLevel(logging.DEBUG)
 
-    @bot.event
-    async def on_ready():
-        print(f"{bot.name} bot is ready.")
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.WARNING)
+formatter = logging.Formatter("\x1b[38;5;203m[%(levelname)s: %(name)s] %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+handler = logging.FileHandler(filename="logs/discord.log", encoding="utf-8", mode="w")
+handler.setFormatter(
+    logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s")
+)
+logger.addHandler(handler)
 
-    await bot.starting_logic()
+bot = Placeholder(intents=discord.Intents.all(), owner_id=723386696007155763, tree_cls=CommandTree)
+discord.Embed = Embed
+discord.Embed.bot = bot
+discord.ui.View = View
+
+
+
+if __name__ == "__main__":
     try:
-        await bot.start(config["bot"]["token"])
-    except (KeyboardInterrupt, SystemExit):
-        print("\n\nBot has been stopped\n\n")
+        loop = asyncio.new_event_loop()
+        loop.set_debug(True)
+        asyncio.set_event_loop(loop)
+
+        loop.create_task(bot.start(os.getenv("TOKEN")))
+        loop.run_forever()
+
+    except (KeyboardInterrupt, SystemExit, asyncio.CancelledError):
+         bot.logger.critical("Fucking off...")
+
     finally:
-        await bot.closing_logic()
-
-
-try:
-    asyncio.run(main())
-except (KeyboardInterrupt, SystemExit):
-    print("\n\nBot has been stopped\n\n")
+        loop.close()
+        bot.logger.critical("Bot fucked off....")
+        sys.exit()
